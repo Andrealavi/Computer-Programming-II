74c7567b02fe8373f7b73e94ef044e1423f3897f
Andrealavi <andrealavino1410.2003@gmail.com>
2023-06-09 14:34:52 +0200

    Implemented DFS and BFS visit and other functions

--- tree/main.cpp
+++ tree/main.cpp
@@ -1,21 +1,63 @@
 #include <iostream>
+#include <algorithm>
 #include "string.h"
 #include "tree.h"
+#include "coda-bfs.h"
 
 using namespace std;
 
-void printTree(tree t) {
-	if (t == NULL) {
-		return;
+void serialize(tree t) {
+	if (t != NULL) {
+		cout << "(" << getInfo(t);
+
+		if (getFirstChild(t) != NULL) {
+			cout << " ";
+			serialize(getFirstChild(t));
+		}
+
+		cout << ")";
+
+		if (getNextSibling(t) != NULL) {
+			serialize(getNextSibling(t));
+		}
+	}
+}
+
+int getTreeHeight(tree t) {
+	if (t != NULL) {
+		return max(1 + getTreeHeight(getFirstChild(t)),
+				   getTreeHeight(getNextSibling(t)) - 1);
+	}
+}
+
+int dimensionBFS(tree t) {
+	codaBFS q = newQueue();
+
+	q = enqueue(q, t);
+
+	int dim = 0;
+
+	while (!isEmpty(q)) {
+		node* n = dequeue(q);
+
+		dim++;
+
+		if (getNextSibling(n) != NULL) {
+			q = enqueue(q, getNextSibling(n));
+		}
+
+		if (getFirstChild(n) != NULL) {
+			q = enqueue(q, getFirstChild(n));
+		}
 	}
 
-	print(getInfo(t));
-	cout << endl;
+	return dim;
+}
 
-	if (getNextSibling(t) == NULL) {
-		printTree(getFirstChild(t));
-	} else {
-		printTree(getNextSibling(t));
+int dimensionDFS(tree t) {
+	if (t != NULL) {
+		return 1 + dimensionDFS(getFirstChild(t)) +
+			   dimensionDFS(getNextSibling(t));
 	}
 }
 
@@ -25,14 +67,16 @@ int main() {
 	tree c = newNode("Paolo");
 	insertChild(t, c);
 
-	c = newNode("Marco");
-	insertChild(t, c);
-	insertSibling(c, newNode("Lucia"));
+	insertSibling(c, newNode("Marco"));
+	insertChild(c->nextSibling, newNode("Lucia"));
+	insertSibling(c->nextSibling, newNode("Anna"));
 
-	c = newNode("Anna");
-	insertChild(t, c);
+	serialize(t);
+	cout << endl;
 
-	printTree(t);
+	cout << getTreeHeight(t) << endl;
+	cout << dimensionBFS(t) << endl;
+	cout << dimensionDFS(t) << endl;
 
 	return 0;
 }
