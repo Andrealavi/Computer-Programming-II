12d08e6fc2ebcde064b55c9c86d444e1e0a40af5
Andrealavi <andrealavino1410.2003@gmail.com>
2023-06-19 16:02:23 +0200

    Added a function to control whether there is a cycle in the graph or not

--- graph/main.cpp
+++ graph/main.cpp
@@ -94,6 +94,55 @@ bool connected(graph g) {
 	return false;
 }
 
+void dfsVisit(graph g, bool* visited, int* pre, int* post, int& time,
+			  bool& cycle, int i) {
+	if (cycle) {
+		return;
+	}
+
+	adjList l = getAdjList(g, i);
+	visited[i - 1] = true;
+	pre[i - 1] = time++;
+
+	while (l != NULL) {
+		int node = getAdjNode(l) - 1;
+
+		if (!visited[node]) {
+			dfsVisit(g, visited, pre, post, time, cycle, node + 1);
+		} else if (post[node] == 0 && pre[node] < pre[i - 1]) {
+			cycle = true;
+			return;
+		}
+
+		l = getNextAdj(l);
+	}
+
+	post[i - 1] = time++;
+}
+
+bool isThereCycle(graph g) {
+	bool* visited = new bool[g.dim];
+	int* pre = new int[g.dim];
+	int* post = new int[g.dim];
+	int time = 1;
+
+	bool cycle = false;
+
+	for (int i = 0; i < g.dim; i++) {
+		visited[i] = false;
+		pre[i] = 0;
+		post[i] = 0;
+	}
+
+	for (int i = 1; i <= g.dim; i++) {
+		if (!visited[i - 1]) {
+			dfsVisit(g, visited, pre, post, time, cycle, i);
+		}
+	}
+
+	return cycle;
+}
+
 int main(int argc, char* argv[]) {
 	ifstream file;
 	bool* visited;
@@ -102,9 +151,11 @@ int main(int argc, char* argv[]) {
 
 	graph g = gBuild(file, atoi(argv[2]), atoi(argv[3]));
 
-	iterativeBFS(g, 1, visited);
+	// iterativeBFS(g, 1, visited);
 
-	cout << connected(g) << endl;
+	// cout << connected(g) << endl;
+
+	cout << isThereCycle(g) << endl;
 
 	return 0;
 }
